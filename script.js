3af014daa6f127f230d4488c3a99ce5d878492e8
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-06-14 14:26:35 -0500

    added mock generated password

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate password
+function generatePassword() {
+  return "gooddogs35"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
